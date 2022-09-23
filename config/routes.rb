1e96315bfbdd4786f624149c6014f4ca857c99a9
Cedric Le Mercier <cedric.lemercier@outlook.com>
2022-09-23 15:18:10 +0700

    Fix routing and fix XHTML validation link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'news/show'
-  get 'main/index'
   get '/news', to: 'news#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'main#index'
+  root 'site#index'
 end
